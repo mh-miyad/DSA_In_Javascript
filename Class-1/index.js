552fba2e908bd21a7616e50cc6264ceefca5ecc2
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-01-02 10:56:47 +0600

    array

--- Class-1/index.js
+++ Class-1/index.js
@@ -1,31 +1,31 @@
-// console.log("===============Array traversing Normal Way ===================");
-// const arr = [10, 20, 30, 40, 50];
-// for (let i = 0; i < arr.length; i++) {
-//   console.log(arr[i]);
-// }
-// console.log("===============Array traversing in reverse===================");
+console.log("===============Array traversing Normal Way ===================");
+const arr = [10, 20, 30, 40, 50];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+console.log("===============Array traversing in reverse===================");
 // Array traversing in reverse here -1 concept is that is  array length is 5 then it will start from 0 to 4 so that use -1
-// for (let i = arr.length - 1; i >= 0; i--) {
-//   console.log(arr[i]);
-// }
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(arr[i]);
+}
 
 console.log("===============Array traversing  ===================");
-const arr = [10, 20, 30, 40, 50];
-// arr.forEach((element, index) => {
-//   // console.log(element, index); console it will print element and index
-//   // apply  here condition as i want
-//   if (element % 2 === 0) {
-//     console.log(arr[index]);
-//   }
-// });
+
+arr.forEach((element, index) => {
+  // console.log(element, index); console it will print element and index
+  // apply  here condition as i want
+  if (element % 2 === 0) {
+    console.log(arr[index]);
+  }
+});
 
 //* for..of loop
 // It Should use when i do not want to use index
 // **
 
-// for (const el of arr) {
-//   console.log(el);
-// }
+for (const el of arr) {
+  console.log(el);
+}
 
 // real case od use
 
