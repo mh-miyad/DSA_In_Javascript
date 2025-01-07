b02953042e17c0872888e5f2fb86bb323da7e0de
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2025-01-07 22:48:04 +0600

    done problem

--- DSA_Q/1.js
+++ DSA_Q/1.js
@@ -18,3 +18,51 @@
 // Input: words = ["blue","green","bu"]
 // Output: []
 // Explanation: No string of words is substring of another string.
+
+var stringMatching = function (words) {
+  const result = [];
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words.length; j++) {
+      if (i !== j && words[i].includes(words[j])) {
+        if (!result.includes(words[j])) {
+          result.push(words[j]);
+        }
+      }
+    }
+  }
+  return result;
+};
+// var stringMatching = function (words) {
+//   let res = [];
+//   let set1 = new Set();
+//   for (let i of words) {
+//     if (!set1.has(i)) {
+//       set1.add(i);
+//     }
+//   }
+//   let new_words = [];
+//   for (let i of set1) {
+//     new_words.push(i);
+//   }
+//   for (let i of new_words) {
+//     for (let j = 0; j < new_words.length; j++) {
+//       if (new_words[j].includes(i) && new_words[j] !== i) {
+//         res.push(i);
+//       }
+//     }
+//   }
+//   let set2 = new Set();
+//   for (let i of res) {
+//     if (!set2.has(i)) {
+//       set2.add(i);
+//     }
+//   }
+//   let new_words2 = [];
+//   for (let i of set2) {
+//     new_words2.push(i);
+//   }
+//   return new_words2;
+// };
+
+const words = ["mass", "as", "hero", "superhero"];
+console.log(stringMatching(words));
